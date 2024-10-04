1adc51d8e5d65a6f040ea33217213197618f49d0
Egor <example@gmail.com>
2024-10-05 00:24:57 +0300

    Условия

--- Preview/js/script.js
+++ Preview/js/script.js
@@ -1,21 +1,36 @@
   "use strict";
 
-  const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+if (1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
 
-  const personalMovieDB = {
-    count: numberOfFilms, 
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-  };
 
-  const a = prompt('Один из последних просмотренныз фильмов?', ''),
-        b = prompt('На сколько оцените егo?', ''),
-        c = prompt('Один из последних просмотренныз фильмов?', ''),
-        d = prompt('На сколько оцените егo?', '');
 
- personalMovieDB.movies[a] = b;
- personalMovieDB.movies[c] = d;
+/* if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
 
- console.log( personalMovieDB);
+(num === 50) ? console.log('Ok!') : console.log('Error'); */
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('ДАААА');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
