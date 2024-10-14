afea3cfb169195ca99468d5977de1d1b95aa2a7a
Egor <example@gmail.com>
2024-10-14 16:04:51 +0300

    Получение элементов со страницы

--- Preview/js/script.js
+++ Preview/js/script.js
@@ -1,53 +1,22 @@
   "use strict";
-  // To string
 
-  // 1)
-  console.log(typeof(String(null)));
-  console.log(typeof(String(4)));
+  const box = document.getElementById('box'); //обращение к элементу напрямую
 
-  // 2)
-  console.log(typeof(null + ''));
+  console.log(box);
 
-  const num = 5;
+  const btns = document.getElementsByTagName('button');
 
-  console.log("https://vk.com/catalog/" + num);
+  console.log(btns[0]);
 
-  const fontSize = 26 + 'px';
+  const circles = document.getElementsByClassName('circle'); // тут сверху и снизу создается псевдомассив
+  console.log(circles);
 
-  // To number
+  const hearts = document.querySelectorAll('.heart'); //точка нужна чтобы показать что мы работаем с классом
 
-// 1)
-console.log(typeof(Number('4')));
+hearts.forEach(item => {
+  console.log(item);
+});
 
-// 2)
-console.log(typeof(+ '5'));
+const oneHeart = document.querySelector('.heart'); ////обращение к элементу напрямую
+console.log(oneHeart);
 
-// 3) 
-console.log(typeof(parseInt("15px", 10)));
-
-let answ = +prompt("Hello", "");
-
-// To boolean
-
-// 0, '', null, undefined, NaN;
-
-//1)
-let switcher = null;
-
-if (switcher) {
-  console.log('Working...')
-}
-
-switcher = 1;
-
-if (switcher) {
-  console.log('Working...')
-}
-
-//2)
-
-console.log(typeof(Boolean('4')));
-
-//3)
-
-console.log(typeof(!!"44444"));
